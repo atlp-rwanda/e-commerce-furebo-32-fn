93accfc2a99d06d7fcc7c8ae94c0a7d02dd0ef95
mugishaj092 <mugishajoseph092@gmail.com>
2024-07-03 13:19:37 +0200

    will add materialUI setup

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,14 @@
+import Button from '@mui/material/Button';
+
 function Home() {
   return (
-    <div className="bg-[blue]">
+    <div className="bg-blue-500 p-4">
       Home GeekLords
+      <Button className="bg-red-500 text-black hover:bg-red-700" variant="contained">
+        Contained
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import "./globals.css"
 import "./styles/main.scss";
 import "./styles/secondary.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // The exclamation mark is a non-null assertion
