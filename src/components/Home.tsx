fedfdac928b66c61d27f4879962b4d9965bf6cd0
Mugisha146 <emmyzizo1@gmail.com>
2024-07-09 10:23:58 +0200

    Users should be able to Login with Email and Password

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,30 @@ import Popular from "./product/Popular";
 import Product from "./product/product";
 import CardSlider from "./showcase/slider/showcase";
 
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 function Home() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/login");
+  };
   return (
-    <div className="w-full h-full flex flex-col justify-center">
-      <CardSlider />
-      <Popular />
-      <ChooseUs />
-      <Product />
+    <div className="bg-blue-500 p-4">
+      Home GeekLords
+      <Button
+        className="bg-red-500 text-black hover:bg-red-700"
+        variant="contained"
+        onClick={handleClick}
+      >
+        Login
+      </Button>
+      <div className="w-full h-full flex flex-col justify-center">
+        <CardSlider />
+        <Popular />
+        <ChooseUs />
+        <Product />
+      </div>
     </div>
   );
 }
---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+interface ProtectedRouteProps {
+  children: JSX.Element;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
