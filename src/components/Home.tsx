7315628032115425240c4fba8a6cdf949bbaa70c
Mugisha146 <emmyzizo1@gmail.com>
2024-07-10 18:39:22 +0200

    remove MaterializeCSS

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,33 +1,19 @@
+import Login from "../components/Login";
 import ChooseUs from "./product/ChooseUs";
 import Popular from "./product/Popular";
 import Product from "./product/product";
 import CardSlider from "./showcase/slider/showcase";
 
-import Button from "@mui/material/Button";
-import { useNavigate } from "react-router-dom";
-function Home() {
-  const navigate = useNavigate();
 
-  const handleClick = () => {
-    navigate("/login");
-  };
+function Home() {
   return (
-    <div className="bg-blue-500 p-4">
-      Home GeekLords
-      <Button
-        className="bg-red-500 text-black hover:bg-red-700"
-        variant="contained"
-        onClick={handleClick}
-      >
-        Login
-      </Button>
-      <div className="w-full h-full flex flex-col justify-center">
+    <div className="w-full h-full flex flex-col justify-center">
+      <Login />
         <CardSlider />
         <Popular />
         <ChooseUs />
         <Product />
       </div>
-    </div>
   );
 }
 
