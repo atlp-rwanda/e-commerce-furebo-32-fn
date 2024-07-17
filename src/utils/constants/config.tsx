d2839aca9b4663cff533e6796a5a2e3dab2c5391
“mugishaj092” <mugishajoseph092@gmail.com>
2024-07-17 11:49:12 +0200

    Fix backend URL

--- src/utils/constants/config.tsx
+++ src/utils/constants/config.tsx
@@ -1,3 +1,3 @@
 export const BASE_API_URL =
-'https://e-commerce-furebo-32-bn-1.onrender.com/api/';
+'https://e-commerce-furebo-32-bn-1.onrender.com/';
 // export const BASE_API_URL = 'http://localhost:5001/';
